b884f69ed66a46ced2b1d337910abf79e9eee141
yudian.zy <yudian.zy@antgroup.com>
2025-08-06 12:37:58 +0800

    [theta] merge 0806-3

--- python/sglang/srt/models/bailing_moe.py
+++ python/sglang/srt/models/bailing_moe.py
@@ -60,7 +60,7 @@ from sglang.srt.layers.linear import (
 )
 from sglang.srt.layers.logits_processor import LogitsProcessor
 from sglang.srt.layers.moe.ep_moe.layer import get_moe_impl_class
-from sglang.srt.layers.moe.ep_moe.token_dispatcher import DeepEPDispatcher
+from sglang.srt.layers.moe.token_dispatcher import DeepEPDispatcher
 from sglang.srt.layers.moe.topk import TopK
 from sglang.srt.layers.quantization.base_config import QuantizationConfig
 from sglang.srt.layers.radix_attention import RadixAttention
