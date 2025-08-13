a79b759207d250e684ac950c14df2fd98c9a2f4b
霜林 <augusto.yjh@antgroup.com>
2025-08-13 11:23:28 +0800

    PullRequest: 210 do not return -1 when source .xccl_env.sh with env NCCL* already set
    Merge branch 'install-flashinfer-python-without-isolated-env of git@code.alipay.com:Theta/SGLang.git into main
    
    https://code.alipay.com/Theta/SGLang/pull_requests/210
    
    
    Reviewed-by: 商元 <cx483263@antgroup.com>
    
    
    * do not return -1 when source .xccl_env.sh with env NCCL* already set

--- docker/ant/.xccl_env.sh
+++ docker/ant/.xccl_env.sh
@@ -1,28 +1,28 @@
 # default xccl envs for H20 from https://yuque.antfin.com/hegpb4/kg7h1z/rt5tbnz6bzzkmivw
 
-[ -z ${NCCL_SOCKET_IFNAME+x} ] && export NCCL_SOCKET_IFNAME=bond0
-[ -z ${NCCL_IB_HCA+x} ] && export NCCL_IB_HCA=mlx5_bond
-[ -z ${NVSHMEM_HCA_PE_MAPPING+x} ] && export NVSHMEM_HCA_PE_MAPPING="mlx5_bond_0:1:2,mlx5_bond_1:1:2,mlx5_bond_2:1:2,mlx5_bond_3:1:2"
-[ -z ${GLOO_SOCKET_IFNAME+x} ] && export GLOO_SOCKET_IFNAME=eth0
+if [ -z ${NCCL_SOCKET_IFNAME+x} ]; then export NCCL_SOCKET_IFNAME=bond0;fi
+if [ -z ${NCCL_IB_HCA+x} ]; then export NCCL_IB_HCA=mlx5_bond;fi
+if [ -z ${NVSHMEM_HCA_PE_MAPPING+x} ]; then export NVSHMEM_HCA_PE_MAPPING="mlx5_bond_0:1:2,mlx5_bond_1:1:2,mlx5_bond_2:1:2,mlx5_bond_3:1:2";fi
+if [ -z ${GLOO_SOCKET_IFNAME+x} ]; then export GLOO_SOCKET_IFNAME=eth0;fi
 
-[ -z ${NCCL_NET_PLUGIN+x} ] && export NCCL_NET_PLUGIN=''
-[ -z ${NCCL_IB_GID_INDEX+x} ] && export NCCL_IB_GID_INDEX=3
-[ -z ${NCCL_IB_TIMEOUT+x} ] && export NCCL_IB_TIMEOUT=22
-[ -z ${NCCL_IB_RETRY_CNT+x} ] && export NCCL_IB_RETRY_CNT=7
-[ -z ${NCCL_IB_SL+x} ] && export NCCL_IB_SL=5
-[ -z ${NCCL_IB_TC+x} ] && export NCCL_IB_TC=136
-[ -z ${NCCL_DEBUG+x} ] && export NCCL_DEBUG=INFO
-[ -z ${NCCL_SET_THREAD_NAME+x} ] && export NCCL_SET_THREAD_NAME=1
-[ -z ${NCCL_IB_QPS_PER_CONNECTION+x} ] && export NCCL_IB_QPS_PER_CONNECTION=8
-[ -z ${NCCL_SET_THREAD_NAME+x} ] && export NCCL_SET_THREAD_NAME=1
-[ -z ${NCCL_DEBUG_SUBSYS+x} ] && export NCCL_DEBUG_SUBSYS=INIT,TUNING,GRAPH
+if [ -z ${NCCL_NET_PLUGIN+x} ]; then export NCCL_NET_PLUGIN='';fi
+if [ -z ${NCCL_IB_GID_INDEX+x} ]; then export NCCL_IB_GID_INDEX=3;fi
+if [ -z ${NCCL_IB_TIMEOUT+x} ]; then export NCCL_IB_TIMEOUT=22;fi
+if [ -z ${NCCL_IB_RETRY_CNT+x} ]; then export NCCL_IB_RETRY_CNT=7;fi
+if [ -z ${NCCL_IB_SL+x} ]; then export NCCL_IB_SL=5;fi
+if [ -z ${NCCL_IB_TC+x} ]; then export NCCL_IB_TC=136;fi
+if [ -z ${NCCL_DEBUG+x} ]; then export NCCL_DEBUG=INFO;fi
+if [ -z ${NCCL_SET_THREAD_NAME+x} ]; then export NCCL_SET_THREAD_NAME=1;fi
+if [ -z ${NCCL_IB_QPS_PER_CONNECTION+x} ]; then export NCCL_IB_QPS_PER_CONNECTION=8;fi
+if [ -z ${NCCL_SET_THREAD_NAME+x} ]; then export NCCL_SET_THREAD_NAME=1;fi
+if [ -z ${NCCL_DEBUG_SUBSYS+x} ]; then export NCCL_DEBUG_SUBSYS=INIT,TUNING,GRAPH;fi
 
-[ -z ${NVSHMEM_DEBUG+x} ] && export NVSHMEM_DEBUG=INFO
-[ -z ${NVSHMEM_DEBUG_SUBSYS+x} ] && export NVSHMEM_DEBUG_SUBSYS=INIT
-[ -z ${NVSHMEM_IB_GID_INDEX+x} ] && export NVSHMEM_IB_GID_INDEX=3
-[ -z ${NVSHMEM_IB_TRAFFIC_CLASS+x} ] && export NVSHMEM_IB_TRAFFIC_CLASS=16
-[ -z ${NVSHMEM_IB_SL+x} ] && export NVSHMEM_IB_SL=5
-[ -z ${NVSHMEM_ENABLE_NIC_PE_MAPPING+x} ] && export NVSHMEM_ENABLE_NIC_PE_MAPPING=1
+if [ -z ${NVSHMEM_DEBUG+x} ]; then export NVSHMEM_DEBUG=INFO;fi
+if [ -z ${NVSHMEM_DEBUG_SUBSYS+x} ]; then export NVSHMEM_DEBUG_SUBSYS=INIT;fi
+if [ -z ${NVSHMEM_IB_GID_INDEX+x} ]; then export NVSHMEM_IB_GID_INDEX=3;fi
+if [ -z ${NVSHMEM_IB_TRAFFIC_CLASS+x} ]; then export NVSHMEM_IB_TRAFFIC_CLASS=16;fi
+if [ -z ${NVSHMEM_IB_SL+x} ]; then export NVSHMEM_IB_SL=5;fi
+if [ -z ${NVSHMEM_ENABLE_NIC_PE_MAPPING+x} ]; then export NVSHMEM_ENABLE_NIC_PE_MAPPING=1;fi
 
-[ -z ${IB_DEVICE_LIST+x} ] && export IB_DEVICE_LIST="mlx5_bond_0,mlx5_bond_1,mlx5_bond_2,mlx5_bond_3"
+if [ -z ${IB_DEVICE_LIST+x} ]; then export IB_DEVICE_LIST="mlx5_bond_0,mlx5_bond_1,mlx5_bond_2,mlx5_bond_3";fi
 
