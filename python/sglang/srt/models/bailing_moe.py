c9b2b257c82ff1874b8d6a9a513ffb22a8c9f21f
yudian.zy <yudian.zy@antgroup.com>
2025-08-18 11:37:05 +0800

    [theta-bailing] bailing-fix

--- python/sglang/srt/models/bailing_moe.py
+++ python/sglang/srt/models/bailing_moe.py
@@ -72,7 +72,7 @@ from sglang.srt.layers.vocab_parallel_embedding import (
     VocabParallelEmbedding,
 )
 from sglang.srt.managers.schedule_batch import global_server_args_dict
-from sglang.srt.model_executor.cuda_graph_runner import get_is_capture_mode
+from sglang.srt.model_executor.graph_runner import get_is_capture_mode
 from sglang.srt.model_executor.forward_batch_info import (
     ForwardBatch,
     ForwardMode,
