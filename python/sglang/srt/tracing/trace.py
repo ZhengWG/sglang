e781310597687da969709042e8ca9a2d1436066d
羽癫 <yudian.zy@antgroup.com>
2025-11-13 11:40:58 +0800

    Merge remote-tracking branch 'public/main' into sglang_public_tracker
    # Conflicts:
    #	python/sglang/srt/managers/tokenizer_manager.py
    #	python/sglang/srt/tracing/trace.py

--- python/sglang/srt/tracing/trace.py
+++ python/sglang/srt/tracing/trace.py
@@ -490,8 +490,7 @@ def trace_req_start(
     bootstrap_room_span_context = None
 
     external_trace_context = _trace_context_propagator.extract(
-                external_trace_header
-            )
+        external_trace_header)
 
     # create bootstrap room span
     if tracing_multispan_enabled:
