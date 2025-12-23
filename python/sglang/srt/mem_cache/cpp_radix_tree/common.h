84e221cb03c650e78d7b9faf1433dbe7a9b7c579
得泽 <zhangkaihong.zkh@antgroup.com>
2025-12-23 11:46:18 +0800

    PullRequest: 513 修复gcc10.2.1编译问题并更新pyproject.toml以包含cpp_radix_tree文件
    Merge branch 'deze/fix_cpp_radix of git@code.alipay.com:Theta/SGLang.git into sglang_public_tracker
    
    https://code.alipay.com/Theta/SGLang/pull_requests/513
    
    
    Reviewed-by: 文贤 <luoyuan.luo@antgroup.com>
    
    
    * add cpp files for cpp_radix_tree to pyproject.toml. (#14052)
    * fix compilation with gcc10.2.1
    * add c++20 check

--- python/sglang/srt/mem_cache/cpp_radix_tree/common.h
+++ python/sglang/srt/mem_cache/cpp_radix_tree/common.h
@@ -1,7 +1,13 @@
 #pragma once
 #include <cstddef>
 #include <cstdint>
+#if __cplusplus < 202002L
+#include <experimental/source_location>
+using source_location = std::experimental::source_location;
+#else
 #include <source_location>
+using source_location = std::source_location;
+#endif
 #include <span>
 #include <stdexcept>
 #include <string>
@@ -15,10 +21,8 @@ using token_slice = std::span<const token_t>;
 using NodeHandle = std::size_t;
 using IOTicket = std::uint32_t;
 
-inline void _assert(
-    bool condition,
-    const char* message = "Assertion failed",
-    std::source_location loc = std::source_location::current()) {
+inline void
+_assert(bool condition, const char* message = "Assertion failed", source_location loc = source_location::current()) {
   if (!condition) [[unlikely]] {
     std::string msg = message;
     msg = msg + " at " + loc.file_name() + ":" + std::to_string(loc.line()) + " in " + loc.function_name();
